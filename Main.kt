cd7e34a906cdf87693b2ac1b71f8b8f16d803752
JonathanPepi <144859139+JonathanPepi@users.noreply.github.com>
2024-08-22 11:46:34 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,21 @@
+import ListaXP.*
+fun main() {
+
+    var Ex01 = Ex01()
+    var Ex02 = Ex02()
+    var Ex03 = Ex03()
+    var Ex04 = Ex04()
+    var Ex05 = Ex05()
+    var Ex06 = Ex06()
+    var Ex07 = Ex07()
+    var Ex08 = Ex08()
+
+    //Ex01.main()
+    //Ex02.main()
+    //Ex03.main()
+    //Ex04.main()
+    //Ex05.main()
+    //Ex06.main()
+    //SEx07.main()
+    //Ex08.main()
+    }
